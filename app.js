7639922fc1b2a59a3e58589395bc2a5fbc36bf3a
Luisfer Guevara <luisferguevaraa@gmail.com>
2023-01-12 00:01:01 +0100

    FEAT: added function pass2 to render info and save second chosen value to generate Code. Added default value

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const renderLogo = () => {
   main$$.appendChild(divLogo$$);
 };
 
-const renderFooter = () => { 
+const renderFooter = () => {
   const footer$$ = document.createElement("footer");
   const footerParagraph$$ = document.createElement("p");
   const footerParagraph2$$ = document.createElement("p");
@@ -31,7 +31,7 @@ const renderFooter = () => {
 let codeGenerator = {};
 
 const pass1 = () => {
-    const div$$ = document.createElement("div");
+  const div$$ = document.createElement("div");
   const paragraphIntro$$ = document.createElement("p");
   const h1$$ = document.createElement("h1");
   const h2$$ = document.createElement("h2");
@@ -67,16 +67,11 @@ const pass1 = () => {
   const fieldset$$ = document.createElement("fieldset");
 
   datesList$$.appendChild(fieldset$$);
-
   datesList$$.classList.add("form--list");
-  //   datesList$$.classList.add("dates-list");
+
   for (let i = 0; i < dates.length; i++) {
-    // const element = dates[i];
     const input$$ = document.createElement("input");
     const label$$ = document.createElement("label");
-    // const defaultValue$$ = dates[0];
-
-    // defaultValue$$.checked = true;
 
     input$$.classList.add("form--input");
     input$$.setAttribute("type", "radio");
@@ -84,6 +79,11 @@ const pass1 = () => {
     input$$.value = dates[i];
     label$$.setAttribute("for", dates[i]);
     label$$.textContent = dates[i];
+    
+    if (i === 0) {
+        input$$.checked = true;
+      }
+  
 
     label$$.appendChild(input$$);
     fieldset$$.appendChild(label$$);
@@ -96,7 +96,7 @@ const pass1 = () => {
   button$$.appendChild(arrowDiv$$);
   wrapper$$.appendChild(button$$);
   div$$.appendChild(wrapper$$);
-  main$$.appendChild(div$$)
+  main$$.appendChild(div$$);
 
   const checkInputs$$ = document.querySelectorAll("input");
   button$$.addEventListener("click", () => {
@@ -105,27 +105,27 @@ const pass1 = () => {
         codeGenerator.stepOne = checkInput.value;
       }
     }
-    main$$.removeChild(div$$)
-    pass2()
-
+    main$$.removeChild(div$$);
+    pass2();
   });
 };
 const pass2 = () => {
+  const div$$ = document.createElement("div");
   const paragraphIntro$$ = document.createElement("p");
   const h1$$ = document.createElement("h1");
-  const h2$$ = document.createElement("h2");
 
-  paragraphIntro$$.textContent = "paso 1 de 2";
-  h1$$.textContent = "¡VAMOS HOSTIA!";
-  h2$$.classList.add("main--paragraph");
-  h2$$.textContent =
-    "Has llegado hasta el test de Siroko. Responde las sigueinte preguntas y genera tu código premiado a medida";
+  paragraphIntro$$.textContent = "paso 2 de 2";
+  h1$$.textContent = "¡VAMOS, UNA MÁS!";
 
-  main$$.appendChild(paragraphIntro$$);
-  main$$.appendChild(h1$$);
-  main$$.appendChild(h2$$);
+  div$$.appendChild(paragraphIntro$$);
+  div$$.appendChild(h1$$);
 
-  const dates = ["2016", "2017", "2018", "2019", "2020", "2021"];
+  const alternatives = [
+    "Segaría a navaja",
+    "Rechazaría un cachopo",
+    "Renunciaría a mis tierras",
+    "Regalaría una ternera",
+  ];
 
   const wrapper$$ = document.createElement("div");
   const p$$ = document.createElement("p");
@@ -136,54 +136,55 @@ const pass2 = () => {
 
   wrapper$$.classList.add("form--wrapper");
   p$$.classList.add("form--title");
-  p$$.textContent = "Uso Siroko desde ...";
+  p$$.textContent = "por unas gas siroko, yo...";
   button$$.classList.add("form--button");
   buttonText$$.textContent = "Siguiente";
   arrowDiv$$.classList.add("form--button__arrow-div");
   arrow$$.classList.add("form--button__arrow-img");
 
-  const datesList$$ = document.createElement("form");
+  const alternativesList$$ = document.createElement("form");
   const fieldset$$ = document.createElement("fieldset");
 
-  datesList$$.appendChild(fieldset$$);
+  alternativesList$$.appendChild(fieldset$$);
 
-  datesList$$.classList.add("form--list");
-  //   datesList$$.classList.add("dates-list");
-  for (let i = 0; i < dates.length; i++) {
+  alternativesList$$.classList.add("form--list");
+
+  for (let i = 0; i < alternatives.length; i++) {
     // const element = dates[i];
     const input$$ = document.createElement("input");
     const label$$ = document.createElement("label");
-    // const defaultValue$$ = dates[0];
-
-    // defaultValue$$.checked = true;
 
     input$$.classList.add("form--input");
     input$$.setAttribute("type", "radio");
     input$$.setAttribute("name", "option");
-    input$$.value = dates[i];
-    label$$.setAttribute("for", dates[i]);
-    label$$.textContent = dates[i];
+    input$$.value = alternatives[i];
+    label$$.setAttribute("for", alternatives[i]);
+    label$$.textContent = alternatives[i];
+
+    if (i === 0) {
+      input$$.checked = true;
+    }
 
     label$$.appendChild(input$$);
     fieldset$$.appendChild(label$$);
   }
 
   wrapper$$.appendChild(p$$);
-  wrapper$$.appendChild(datesList$$);
+  wrapper$$.appendChild(alternativesList$$);
   arrowDiv$$.appendChild(arrow$$);
   button$$.appendChild(buttonText$$);
   button$$.appendChild(arrowDiv$$);
   wrapper$$.appendChild(button$$);
-  main$$.appendChild(wrapper$$);
+  div$$.appendChild(wrapper$$);
+  main$$.appendChild(div$$);
 
   const checkInputs$$ = document.querySelectorAll("input");
   console.log(checkInputs$$);
   console.log(codeGenerator);
   button$$.addEventListener("click", () => {
-    h1$$.textContent = "jjja";
     for (const checkInput of checkInputs$$) {
       if (checkInput.checked == true) {
-        codeGenerator.stepOne = checkInput.value;
+        codeGenerator.stepTwo = checkInput.value;
       }
     }
     console.log(codeGenerator);
@@ -194,6 +195,5 @@ const init = () => {
   renderLogo();
   renderFooter();
   pass1();
-
 };
 window.onload = init;
